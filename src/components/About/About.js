85c4ca5387d5bf4e34998ec9a58435e26c64e899
Diana Vlasenkova <dianadomino24work@gmail.com>
2021-01-10 18:38:32 +0300

    add raw about, invert header scroll, fix container paddings

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,25 @@
 import './About.css';
+import Container from '../Container/Container';
+import author from '../../images/diana.jpg';
+import { texts } from '../../utils/constants';
 
 function About() {
   return (
-    <section className="about">
-      <h1>bbgg</h1>
-      <h1>bbgg</h1>
-      <h1>bbgg</h1>
-    </section>
+    <Container>
+      <section className="about">
+        <img className="about__photo" src={author} alt='Author photo'/>
+        <div className="about__description">
+          <h2 className="about__title">About me</h2>
+          <p className="about__text">
+            {texts.aboutDescription1}
+          </p>
+          <p className="about__text">
+            {texts.aboutDescription2}
+          </p>
+        </div>
+
+      </section>
+    </Container>
   );
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const texts = {
+  aboutDescription1: 'Hi, I\'m Diana and I enjoy programming! '
+    + 'I’m a frontend developer, Practicum by Yandex graduate, '
+    + 'having 10 months experience at various web-development projects '
+    + '(MERN stack), including a team lead experience at a promotion '
+    + 'project for children’s bookshop. For me it\'s really essential'
+    + ' to improve my skills with every project, so I’m dreaming to'
+    + ' gain experience in an international developing company in an'
+    + ' ambitious team.',
+  aboutDescription2: 'I\'m also an avid trekker and nature lover. I love '
+    + 'listening to audiobooks and lectures on design, art, architecture '
+    + 'and neuropsychology, making pottery and listening to jazz and '
+    + 'classical music.',
+};
+export const BASE_URL = 'https://www.api.dianadomino24.students.nomoreparties.space';
