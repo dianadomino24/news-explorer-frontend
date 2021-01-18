7af62cd09f58acf242526fac35ea861a095e320c
Diana Vlasenkova <dianadomino24work@gmail.com>
2021-01-18 21:54:02 +0300

    add mainApi,newsApi raw

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,31 +1,24 @@
-// import { BASE_URL } from './utils';
+import { BASE_URL, HEADERS } from './config';
 
-// const getResponseData = (res) => res.json()
-// .then((json) => (res.ok ? json : Promise.reject(json)));
+const getResponseData = (res) => res.json()
+  .then((json) => (res.ok ? json : Promise.reject(json)));
 
-// export const register = (email, password) => fetch(`${BASE_URL}/signup`, {
-//   method: 'POST',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({ email, password }),
-// }).then((res) => getResponseData(res));
+export const register = (email, password, name) => fetch(`${BASE_URL}/signup`, {
+  method: 'POST',
+  headers: HEADERS,
+  body: JSON.stringify({ email, password, name }),
+}).then((res) => getResponseData(res));
 
-// export const authorize = (email, password) => fetch(`${BASE_URL}/signin`, {
-//   method: 'POST',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({ email, password }),
-// }).then((res) => getResponseData(res));
+export const authorize = (email, password) => fetch(`${BASE_URL}/signin`, {
+  method: 'POST',
+  headers: HEADERS,
+  body: JSON.stringify({ email, password }),
+}).then((res) => getResponseData(res));
 
-// export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
-//   method: 'GET',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//     Authorization: `Bearer ${token}`,
-//   },
-// }).then((res) => getResponseData(res));
+export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
+  method: 'GET',
+  headers: {
+    HEADERS,
+    Authorization: `Bearer ${token}`,
+  },
+}).then((res) => getResponseData(res));
